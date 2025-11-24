dae7b359a084c3ffdb06f33b93abc2505ac8b48d
ASUS <ptharanan@gmail.com>
2025-11-24 18:29:02 +0530

    Update settings for production: disable debug mode, set allowed hosts, and adjust static root path

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0=_a*2#j^=*%3&mz0uhsr%qqq-k5_dsu6b$=@5-wto)aib$r=*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,8 +54,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-ALLOWED_HOSTS = ['*']
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -125,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.Path.join(BASE_DIR, 'static')
 
 STORAGES = {
     # ...
